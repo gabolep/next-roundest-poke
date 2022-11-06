6ce17805c75fa0738520bab39c56c662edc66947
gabolep <gabriel.escudero@alumnos.ucn.cl>
2022-11-06 12:29:19 -0300

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className="">asd</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2x1 text-center">Which Pokemon is Rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 }
